4dbd7962e2480e950eb0cf3d7b7f18ee653ebcdd
vijaykr33 <vijaykumar_23se170@dtu.ac.in>
2024-07-01 16:51:31 +0530

    added footer and social-media

--- src/components/Features/Featuro.jsx
+++ src/components/Features/Featuro.jsx
@@ -10,8 +10,8 @@ const Featuro = () => {
     return (
         <div className='flex font-poppins flex-col items-center my-32 space-y-44'>
 
-            <div className='flex w-2/4'>
-                <img src={first} className='-ml-20' style={{ width: '600px', height: '600px' }} />
+            <div className='flex w-3/4'>
+                <img src={first} className='-ml-20' style={{height: '600px' }} />
                 <div className='ml-14'>
                     <h1 className=' text-5xl tracking-wide leading-tight' style={{ fontWeight: 1000 }}>Save Time & reduce stress with effortless parking.</h1>
                     <p className='text-2xl my-3' style={{color: '#3E4E60'}}> Mulpa earum molestiae commodi illum fugit voluptates. In dolor laboriosam, vero molestiae necessitatibus commodi?</p>
@@ -19,16 +19,16 @@ const Featuro = () => {
             </div>
 
 
-            <div className='flex w-2/4'>
+            <div className='flex w-3/4'>
                 <div>
                 <h1 className=' text-5xl tracking-wide leading-tight' style={{ fontWeight: 1000 }}>Guaranteed parking for concerts, events, airports & more.</h1>
                 <p className='text-2xl my-3' style={{color: '#3E4E60'}}> Mulpa earum molestiae commodi illum fugit voluptates. In dolor laboriosam, vero molestiae necessitatibus commodi?</p>
                 </div>
-                <img src={second} className='ml-20' />
+                <img src={second} className='ml-20' style={{height: '600px' }} />
             </div>
 
-            <div className='flex w-2/4'>
-                <img src={third} alt="" />
+            <div className='flex w-3/4'>
+                <img src={third} alt="" style={{height: '600px' }} />
                 <div className='ml-20'>
                 <h1 className=' text-5xl tracking-wide leading-tight' style={{ fontWeight: 1000 }}>Flexible options to fit your needs & budget.</h1>
                 <p className='text-2xl my-3' style={{color: '#3E4E60'}}>Mulpa earum molestiae commodi illum fugit voluptates. In dolor laboriosam, vero molestiae necessitatibus commodi?</p>
@@ -36,18 +36,18 @@ const Featuro = () => {
                 </div>
             </div>
 
-            <div className='flex w-2/4'>
+            <div className='flex w-3/5'>
                 <div className='mt-20'>
                 <h1 className=' text-5xl tracking-wide leading-tight' style={{ fontWeight: 1000 }}>We Are Always Happy To Help You</h1>
                 <p className='text-2xl my-3' style={{color: '#3E4E60'}}> Mulpa earum molestiae commodi illum fugit voluptates. In dolor laboriosam, vero molestiae necessitatibus commodi?</p>
 
                 </div>
-                <img src={fourth} className='ml-20' />
+                <img src={fourth} className='ml-20' style={{height: '600px' }} />
             </div>
 
 
-            <div className='flex w-2/4'>
-                <img src={fifth} alt="" />
+            <div className='flex w-3/4'>
+                <img src={fifth} alt="" style={{height: '600px' }}/>
                 <div className='ml-20'>
                 <h1 className=' text-5xl tracking-wide leading-tight' style={{ fontWeight: 1000 }}>Park for minutes, hours, or days - we have options for every schedule.</h1>
                 <p className='text-2xl my-3' style={{color: '#3E4E60'}}> Mulpa earum molestiae commodi illum fugit voluptates. In dolor laboriosam, vero molestiae necessitatibus commodi?</p>
