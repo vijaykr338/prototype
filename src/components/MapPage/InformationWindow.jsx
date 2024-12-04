ad83cca150b1abcc9302ae7641a591380cf43b85
Vijay Kumar <vijaykumar_23se170@dtu.ac.in>
2024-12-05 00:33:51 +0530

    this fkin took me 5 hours, god send help, anyways the website now works on mobile

--- src/components/MapPage/InformationWindow.jsx
+++ src/components/MapPage/InformationWindow.jsx
@@ -18,11 +18,13 @@ import { FaCar } from "react-icons/fa6";
 import { VscWorkspaceTrusted } from "react-icons/vsc";
 import { MdCurrencyRupee } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import { IoClose } from "react-icons/io5";
 
 
 const InformationWindow = () => {
   const selectedParkingID = useParkingStore((state) => state.selectedParkingID);
   const isInfoWindowOpen = useParkingStore((state) => state.isInfoWindowOpen)
+  const setInfoWindowOpen = useParkingStore((state) => state.setInfoWindowOpen);
   const [infoContent, setInfoContent] = useState(null);
   const placesLib = useMapsLibrary("places");
   const map = useMap();
@@ -68,107 +70,109 @@ const InformationWindow = () => {
   if (!isInfoWindowOpen) return null;
 
   return (
-    <div className='overflow-y-auto h-screen font-roboto'>
-      {infoContent ? (
-        <div className='flex flex-col space-y-4 mx-1'>
-       {console.log(infoContent)}
-       
-       {infoContent.photos && (
-  <Slider {...settings}>
-    {infoContent.photos.map((photo, index) => (
-      <div key={index}>
-        <img
-          src={photo}
-          alt={`${infoContent.name} photo ${index + 1}`}
-          className='max-h-72 w-full object-cover rounded'
-        />
+    <div className="fixed top-0 left-0 w-full h-full px-4 bg-white overflow-y-auto z-50">
+      <div className="w-full bg-white shadow-md p-2 flex justify-between items-center z-60">
+        <h1 className="text-xl font-bold">Parking Information</h1>
+        <button
+          onClick={() => setInfoWindowOpen(false)}
+          className="text-gray-600 font-bold hover:text-gray-800 transition"
+        >
+          <IoClose size={40} />
+        </button>
       </div>
-    ))}
-  </Slider>
-)}
+      <div className='overflow-y-auto h-full mt-1'>
+        {infoContent ? (
+          <div className='flex flex-col space-y-4 mx-1'>
+            {console.log(infoContent)}
+       
+            {infoContent.photos && (
+              <Slider {...settings}>
+                {infoContent.photos.map((photo, index) => (
+                  <div key={index}>
+                    <img
+                      src={photo}
+                      alt={`${infoContent.name} photo ${index + 1}`}
+                      className='max-h-48 w-full object-cover rounded md:max-h-72'
+                    />
+                  </div>
+                ))}
+              </Slider>
+            )}
 
-          <h2 className='font-inter text-4xl font-semibold my-5 '>{infoContent.name}</h2>
-          <span className='flex items-center text-lg'><FaStar className='text-yellow-400 text-2xl mr-4' />{infoContent.rating}</span>
-          <span className='flex items-center text-lg'><FaWalking className='text-2xl mr-4' /> 15 min walk</span>
-          <Link to={""} className='flex items-center text-lg'><PiPhoneCallLight className='text-2xl mr-4' />{infoContent.international_phone_number}</Link>
-          {/* <p className='text-xl'>{infoContent.address}</p> */}
-          <h3 className='text-3xl font-semibold'>Reserve your parking</h3>
-          <div className='flex justify-around py-3 mr-4 border border-black rounded-3xl'>
-            <div className='flex flex-col items-center'>
-              <p className='font-semibold mb-4'>ENTRANCE TIME</p>
-              <p className='font-bold text-3xl'>3:30 PM</p>
-              <p className='font-semibold'>wed, 3 Jul</p>
+            <h2 className='font-inter text-lg font-semibold my-3 md:text-4xl'>{infoContent.name}</h2>
+            <span className='flex items-center text-sm md:text-lg'><FaStar className='text-yellow-400 text-lg md:text-2xl mr-2 md:mr-4' />{infoContent.rating}</span>
+            <span className='flex items-center text-sm md:text-lg'><FaWalking className='text-lg md:text-2xl mr-2 md:mr-4' /> 15 min walk</span>
+            <Link to={""} className='flex items-center text-sm md:text-lg'><PiPhoneCallLight className='text-lg md:text-2xl mr-2 md:mr-4' />{infoContent.international_phone_number}</Link>
+            <h3 className='text-xl md:text-3xl font-semibold'>Reserve your parking</h3>
+            <div className='flex md:flex-row justify-around py-3 mr-4 border border-black rounded-3xl'>
+              <div className='flex flex-col items-center'>
+                <p className='font-semibold mb-2 md:mb-4'>ENTRANCE TIME</p>
+                <p className='font-bold text-xl md:text-3xl'>3:30 PM</p>
+                <p className='font-semibold'>wed, 3 Jul</p>
+              </div>
+              <div className='flex flex-col items-center'>
+                <p className='font-semibold mb-2 md:mb-4'>EXIT TIME</p>
+                <p className='font-bold text-xl md:text-3xl'>5:30 PM</p>
+                <p className='font-semibold'>wed, 3 Jul</p>
+              </div>
             </div>
-            <div className='flex flex-col items-center'>
-              <p className='font-semibold mb-4'>EXIT TIME</p>
-              <p className='font-bold text-3xl'>5:30 PM</p>
-              <p className='font-semibold'>wed, 3 Jul</p>
-            </div>
-          </div>
-          <div className='flex justify-center'>
-            <button className='text-xl font-bold text-white rounded-full px-16 py-4' style={{ backgroundColor: "#0000FF" }}>BOOK NOW</button>
-          </div>
-          <hr />
-          <div>
-            <h3 className='text-3xl font-semibold'>Prices</h3>
-            <table className='table-fixed w-full mt-4'>
-              <thead>
-                <tr>
-                  <th><span className='flex items-center justify-center'><CiClock2 className='text-2xl' /></span></th>
-                  <th><span className='flex items-center justify-center'><MdCurrencyRupee className='text-2xl' /></span></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className='text-center'>
-                  <td>1 Hour</td>
-                  <td>40</td>
-                </tr>
-                <tr className='text-center'>
-                  <td>2 Hour</td>
-                  <td>70</td>
-                </tr>
-                <tr className='text-center'>
-                  <td>3 Hour</td>
-                  <td>100</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <hr />
-          <div className='flex flex-col gap-3 pb-10'>
-            <h3 className='text-3xl font-semibold'>Anemities</h3>
-            <span className='flex items-center text-lg font-semibold'><CiClock2 className='text-2xl mr-4' /> Hours of operation</span>
             <div className='flex justify-center'>
-              {/* <div className='flex justify-center'>
-                <span>Weekdays :</span>
-                <span>8:00 AM to 7:00 PM </span>
-                <div className='flex justify-around'>
-                  <span>Weekdends :</span>
-                  <span>6:00 AM to 9:00 PM </span>
-                </div>
-              </div> */}
-              {infoContent.opening_hours.weekday_text &&
-                <div className="flex flex-col">
-                  {infoContent.opening_hours.weekday_text.map((day, index) => (
-                    <p key={index}>{day}</p>
-                  ))}
-                </div>
-              }
+              <button className='text-lg md:text-xl font-bold text-white rounded-full px-8 md:px-16 py-2 md:py-4' style={{ backgroundColor: "#0000FF" }}>BOOK NOW</button>
             </div>
-            <span className='flex items-center text-lg font-semibold'><FaCreditCard className='text-2xl mr-4' /> Payment Accepted</span>
-            <div className='flex items-center justify-center gap-10'>
-              <p><FaGooglePay className='text-5xl text-green-800' /></p>
-              <p><FaApplePay className='text-5xl' /></p>
-              <p><RiVisaLine className='text-5xl text-blue-700' /></p>
-              <p><SiPhonepe className='text-3xl text-purple-800' /></p>
+            <hr />
+            <div>
+              <h3 className='text-xl md:text-3xl font-semibold'>Prices</h3>
+              <table className='table-fixed w-full mt-4'>
+                <thead>
+                  <tr>
+                    <th><span className='flex items-center justify-center'><CiClock2 className='text-xl md:text-2xl' /></span></th>
+                    <th><span className='flex items-center justify-center'><MdCurrencyRupee className='text-xl md:text-2xl' /></span></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className='text-center'>
+                    <td>1 Hour</td>
+                    <td>40</td>
+                  </tr>
+                  <tr className='text-center'>
+                    <td>2 Hour</td>
+                    <td>70</td>
+                  </tr>
+                  <tr className='text-center'>
+                    <td>3 Hour</td>
+                    <td>100</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
-            <span className='flex items-center text-lg font-semibold'><FaCar className='text-2xl mr-4' /> Outside Parking</span>
-            <span className='flex items-center text-lg font-semibold'><VscWorkspaceTrusted className='text-2xl mr-4' /> 24/7 Security</span>
+            <hr />
+            <div className='flex flex-col gap-3 pb-10'>
+              <h3 className='text-xl md:text-3xl font-semibold'>Anemities</h3>
+              <span className='flex items-center text-sm md:text-lg font-semibold'><CiClock2 className='text-lg md:text-2xl mr-2 md:mr-4' /> Hours of operation</span>
+              <div className='flex justify-center'>
+                {infoContent.opening_hours.weekday_text &&
+                  <div className="flex flex-col">
+                    {infoContent.opening_hours.weekday_text.map((day, index) => (
+                      <p key={index}>{day}</p>
+                    ))}
+                  </div>
+                }
+              </div>
+              <span className='flex items-center text-sm md:text-lg font-semibold'><FaCreditCard className='text-lg md:text-2xl mr-2 md:mr-4' /> Payment Accepted</span>
+              <div className='flex items-center justify-center gap-5 md:gap-10'>
+                <p><FaGooglePay className='text-3xl md:text-5xl text-green-800' /></p>
+                <p><FaApplePay className='text-3xl md:text-5xl' /></p>
+                <p><RiVisaLine className='text-3xl md:text-5xl text-blue-700' /></p>
+                <p><SiPhonepe className='text-2xl md:text-3xl text-purple-800' /></p>
+              </div>
+              <span className='flex items-center text-sm md:text-lg font-semibold'><FaCar className='text-lg md:text-2xl mr-2 md:mr-4' /> Outside Parking</span>
+              <span className='flex items-center text-sm md:text-lg font-semibold'><VscWorkspaceTrusted className='text-lg md:text-2xl mr-2 md:mr-4' /> 24/7 Security</span>
+            </div>
           </div>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 };
